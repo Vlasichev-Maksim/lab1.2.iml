5a055029e65f162f11cc71aaf5697e8623231d6b
Максим <m.vlasichev@mail.ru>
2021-10-01 10:21:30 +0300

    Initial commit

---
+++ src/secound.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class secound {
+    public static void main(String[] args){
+        Scanner cs = new Scanner(System.in);
+        int a=0;
+        a = cs.nextInt();
+        System.out.println("Число:" + a);
+    }
+}
